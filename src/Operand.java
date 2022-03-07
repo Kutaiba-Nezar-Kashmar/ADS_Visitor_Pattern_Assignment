0a3d7a2419921a3cc06194dbc5c8ee37c810ca00
Kasper <kasperscoop@gmail.com>
2022-03-07 15:32:35 +0100

    Implemented more empty classes

--- src/Operand.java
+++ src/Operand.java
@@ -3,6 +3,11 @@ public class Operand extends Token {
     private int value;
 
 
+    public void accept(CalculatorVisitor visitor)
+    {
+
+    }
+
 
     public int getValue()
     {
--- src/Operator.java
+++ src/Operator.java
@@ -2,6 +2,10 @@ public class Operator extends Token {
 
     Operation operation;
 
+    public void accept(CalculatorVisitor visitor)
+    {
+
+    }
 
 
     public Operation getOperation()
