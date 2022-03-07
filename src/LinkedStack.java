7b5d917d3ffd0ecd7c7b68bad62af1c40bdfdbd6
Michael Bui <michaelbui99@hotmail.com>
2022-03-07 15:55:33 +0100

    Created LinkedStack

---
+++ src/LinkedStack.java
@@ -0,0 +1,21 @@
+import java.util.EmptyStackException;
+
+
+public class LinkedStack<T> implements Stack<T> {
+
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(T elm) {
+
+    }
+
+    @Override
+    public T pop() throws EmptyStackException {
+        return null;
+    }
+}
