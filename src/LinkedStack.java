bb0d3f245c0c50ce709b46bb5cdb636cee8a67d6
Michael Bui <michaelbui99@hotmail.com>
2022-03-08 21:25:27 +0100

    Implemented LinkedStack. Added comments to Stack interface

--- src/LinkedStack.java
+++ src/LinkedStack.java
@@ -1,21 +1,29 @@
 import java.util.EmptyStackException;
 
-
+/**
+ * Stack implementation using (Singly) LinkedList.
+ * Pop and Push operations will happen at front of LinkedList for O(1) time complexity.
+ * */
 public class LinkedStack<T> implements Stack<T> {
     LinkedList<T> list = new LinkedList<T>();
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return list.isEmpty();
     }
 
     @Override
     public void push(T elm) {
-
+        list.addToFront(elm);
     }
 
     @Override
     public T pop() throws EmptyStackException {
-        return null;
+        try{
+            return list.removeFirst();
+        }catch(EmptyStackException e){
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,7 +1,24 @@
 import java.util.EmptyStackException;
 
 public interface Stack<T> {
+    /**
+     * Checks if stack is empty
+     * @return true if empty, else false
+     * */
     boolean isEmpty();
+
+    /**
+     * Pushes element to top of stack
+     *
+     * @param elm element to be pushed onto stack
+     * */
     void push(T elm);
+
+    /**
+     * Removes and retrieves element from the top of stack
+     *
+     * @return top element of stack
+     * @throws EmptyStackException if stack is empty when <c>pop()</c> is called
+     * */
     T pop() throws EmptyStackException;
 }
